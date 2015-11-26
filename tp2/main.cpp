59dc7a80e35f590397b099d573ae2a8158109e48
BBS007 <bbs07@laposte.net>
2015-11-26 19:49:40 +0100

    Remise du main dans son etat normal

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -12,17 +12,16 @@ int main(int argc, char** argv) {
 	if(argc>1) {
         data = new Data(argv[1]);
     } else {
-        data = new Data("INSTANCES/la40.dat");
+        data = new Data("INSTANCES/la01.dat");
         //data = new Data("INSTANCES/exemple.dat");
     }
 
 	Bierwith b(data->nbItems_, data->nbMachines_);
     b.shuffle();
 
-
     //data->evaluer(b);
     //data->rechercheLocale(b, 1000);
-    std::cout << data->algorithmeGenetique(1000, 100);
+    std::cout << data->algorithmeGenetique();
 
     delete data;
 
